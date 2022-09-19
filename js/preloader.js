096751ea12b1ccb4851030e0968cefa2639f50d3
YaelMalia <72941843+YaelMalia@users.noreply.github.com>
2022-09-19 09:52:06 -0500

    archivos js

---
+++ js/preloader.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+    ocultarBody();
+    setTimeout(ocultarPre, 4000);
+    setTimeout(mostrarBody, 4001);
+};
+
+
+function ocultarPre(){
+    $("#preloader").fadeOut(250);
+}
+
+function ocultarBody(){
+    document.getElementById("contenido").style.display = "none";
+    document.getElementById("header").style.display = "none";
+    document.getElementById("footer").style.display = "none";
+    document.getElementById("btn-wsp").style.display = "none";
+    
+   
+}
+
+function mostrarBody(){
+    $("#contenido").fadeIn(1000);
+    $("#header").fadeIn(1500);
+    $("#footer").fadeIn(2000);
+    $("#btn-wsp").fadeIn(2000);
+}
