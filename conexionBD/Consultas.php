48878d95e2003ff248cb26f275f99c85968d9639
YaelMalia <72941843+YaelMalia@users.noreply.github.com>
2022-10-30 02:28:27 -0600

    Avances nocturnos :p

---
+++ conexionBD/Consultas.php
@@ -0,0 +1,94 @@
+﻿<?php
+//singleton
+class Usuarios
+{
+    private static $instancia;
+    private $dbh;
+ 
+    private function __construct()
+    {
+        try {
+	    $servidor="localhost";
+	    $base="purificadora";
+	    $usuario="root";
+	    $contrasenia="";
+	    $this->dbh = new PDO('mysql:host='.$servidor.';dbname='.$base, $usuario, $contrasenia);
+            $this->dbh->exec("SET CHARACTER SET utf8");
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage();
+            die();
+        }
+    }
+ 
+    public static function singleton()
+    {
+        if (!isset(self::$instancia)) {
+            $miclase = __CLASS__;
+            self::$instancia = new $miclase;
+        }
+        return self::$instancia;
+    }
+ 
+    public function get_user($correo, $pass)
+    {
+        try {
+            $query = $this->dbh->prepare("SELECT * FROM usuarios WHERE correo LIKE ? AND contra LIKE ?");
+            $query->bindParam(1, $correo);
+            $query->bindParam(2, $pass);
+            $query->execute();
+            return $query->fetchAll();
+            $this->dbh = null;
+        }catch (PDOException $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function insertar($p1)
+    {
+        try {
+            $query = $this->dbh->prepare("INSERT INTO tabla VALUES (?)");
+            $query->bindParam(1, $p1);
+            $query->execute();
+            return $query->fetchAll();
+            $this->dbh = null;
+        }catch (PDOException $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function Borrar($p1)
+    {
+        try {  
+            $query = $this->dbh->prepare("DELETE FROM tabla WHERE campo LIKE ?");
+            $query->bindParam(1, $p1);
+            $query->execute();
+            return $query->fetchAll();
+            $this->dbh = null;
+        }catch (PDOException $e) {
+            $e->getMessage();
+        }
+    }
+ 
+    public function Actualizar($p1,$p2)
+    {
+        try {
+            $query = $this->dbh->prepare("UPDATE tabla SET campo1=? WHERE campo2 LIKE ?");
+            $query->bindParam(1, $p1);
+            $query->bindParam(2, $p2);
+            $query->execute();
+            $this->dbh = null;
+        } catch (PDOException $e) {
+            $e->getMessage();
+        }
+    }
+
+   
+ 
+    public function __clone()
+    {
+        trigger_error('La clonación no es permitida!.', E_USER_ERROR);
+    }
+}
+?>
