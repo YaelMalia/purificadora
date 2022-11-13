d4600cf609281ff3b291e0bb4cab0fa057183f07
YaelMalia <72941843+YaelMalia@users.noreply.github.com>
2022-11-13 12:23:25 -0600

    Actualización de precios con Mongo - Se modificaron archivos: Actualiza_Info.php y Precios.php

--- Actualiza_Info.php
+++ Actualiza_Info.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="js/jquery.js"></script>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/A_precios.css">
     <script src="js/verifRegistrar.js"></script>
@@ -21,6 +23,24 @@
         input[type=number] {
             -moz-appearance: textfield;
         }
+
+        .m-actualizar{
+          display:none;
+        }
+
+        .bt-actualizar{
+          display:none;
+        }
+
+        .boton{
+          transition-duration:0.7s;
+        }
+        .boton:hover{
+          transform:scale(1.03);
+
+          -webkit-box-shadow: 0px 0px 15px -2px #000000; 
+          box-shadow: 0px 0px 15px -2px #000000;
+        }
 </style>
 <body>
   
@@ -33,35 +53,52 @@
               <div class="card">
                 <div class="Contenido">
                   <h2>Actualizar información</h2>
-                    <form action="Registrate.php" method="post">     
+                    <form action="Actualiza_Info.php" method="post">     
 
-                      <div class="inputBx">
-                        <h4>Marca de garrafón </h4>
-                        <input type="text" id = "inputNombreC">
+                      <div class="inputBx" id = "buscador">
+                        
+                      <h4>Marca de garrafón </h4>
+                        <input name="garrafones" id="inputGarrafones" list="listgarrafones">
+                        <datalist id="listgarrafones">
+                          <option>Bonafont</option>
+                          <option>Ciel</option>
+                          <option>E-Pura</option>
+                        </datalist>
+
                       </div>
 
 
-                      <div class="inputBx">
+                      <div class="m-actualizar" id="m-actualizar">
+
+                      <!-- <div class="inputBx">
                         <h4>Descripción</h4>
-                        <input type="text" id = "inputCorreo">
-                      </div>
+                        <input type="text" id = "inputDescripcion">
+                      </div> -->
 
                       <div class="inputBx">
                         <h4>Precio actual / nuevo</span>
-                        <input type="number" id = "inputPass">
+                        <input type="text" id = "inputPrecio">
+                      </div>
+
                       </div>
                       
 
                      
-                      <div class="animated" id="an"> 
+                      <div id="buscarArticulo">
+                      <div class="animated boton" id="an"> 
                        <span style="left: 170px; top:25px;"></span>
                        Buscar 
                       </div>
+                      </div>
 
-                      <div class="animated" id="an"> 
+                     <div class="bt-actualizar" id="bt-actualizar">
+                     
+                     <div class="animated boton" id="an"> 
                         <span style="left: 170px; top:25px;"></span>
                         Actualizar 
-                    </div>
+                      </div>
+
+                     </div>
                         
     
                     </form>              
@@ -71,6 +108,93 @@
         </div>
         <!--FIN COLUMNAS DE LA TARJETA-->
       </div>
+
+        <script type = "text/javascript">
+          var artGlobal = "";
+          $("#buscarArticulo").click(function(){
+            var articulo = $("#inputGarrafones")[0].value;
+            if(articulo!=""){
+              artGlobal = articulo;
+                var parametros = {
+                    "accion":"consultararticulo",
+                    "Articulo":articulo
+                };
+
+                $.post("conexion/consultasNoSQL.php", parametros, function(respuesta){
+                  
+                  if(respuesta!="Artículo no encontrado"){
+
+                    let jsonParseado = JSON.parse(respuesta);
+
+                    document.getElementById("m-actualizar").style.display = "block";
+                    document.getElementById("bt-actualizar").style.display = "block";
+
+                    document.getElementById("buscador").style.display = "none";
+                    document.getElementById("buscarArticulo").style.display = "none";
+
+                    for(const valor in jsonParseado){
+                      document.getElementById("inputPrecio").value = jsonParseado[valor]["Precio"];
+                    }
+
+                    
+                  }else{
+                    alertify.alert("Error", respuesta);
+
+                    document.getElementById("m-actualizar").style.display = "none";
+                    document.getElementById("bt-actualizar").style.display = "none";
+
+                    document.getElementById("buscador").style.display = "block";
+                    document.getElementById("buscarArticulo").style.display = "block";
+                  }
+
+                });
+
+            }else{
+              alertify.alert("Error", "Campos vacios");
+            }
+          });
+
+          $("#bt-actualizar").click(function(){
+            var nuevoPrice = $("#inputPrecio")[0].value;
+
+            if(nuevoPrice!=""){
+
+              var array = {
+                "Articulo":artGlobal,
+                "Precio":nuevoPrice
+            };
+
+            var array3 = {
+                "datos": array
+            };
+
+              var parametros = {
+                "accion": "modificar",
+                "datos": array3,
+                "Articulo": artGlobal
+              };
+
+              $.post("conexion/consultasNoSQL.php", parametros, function(respuesta){
+                
+                alertify.alert("Excelente", respuesta);
+                artGlobal = "";             
+
+                    document.getElementById("m-actualizar").style.display = "none";
+                    document.getElementById("bt-actualizar").style.display = "none";
+
+                    document.getElementById("buscador").style.display = "block";
+                    document.getElementById("buscarArticulo").style.display = "block";
+
+                    $("#inputGarrafones")[0].value = "";
+                    $("#inputPrecio")[0].value = "";
+            });
+
+            }else{
+              alertify.alert("Error", "Campos vacios");
+            }
+
+          });
+        </script>
 
 </body>
 </html>
--- Precios.php
+++ Precios.php
@@ -5,9 +5,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/prices.css">
+
+    <script src="js/jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="css/A_precios.css"> -->
+    <script src="js/verifRegistrar.js"></script>
+    <script src="alertify/alertify.js"></script>
+    <link rel="stylesheet" href="alertify/alertify.css">
+
+
+    <!-- SCRIPTS PARA PRECIOS RECUPERADOS DE MONGO -->
+
+    <script type = "text/javascript">
+
+        
+        const arrayPrecios = [];
+        var parametros = {
+                "accion":"consultartodo"
+            };  
+
+            $.post("conexion/consultasNoSQL.php",
+                parametros, function(respuesta){
+                    
+                    if(respuesta!="No articulos disponibles"){
+
+                        let jsonParseado = JSON.parse(respuesta);                   
+                        for(let valor in jsonParseado){
+                           arrayPrecios.push(jsonParseado[valor]["datos"]["Precio"]);
+                        }
+                        document.getElementById("precioBonafont").innerHTML = arrayPrecios[0];
+                        document.getElementById("precioCiel").innerHTML = arrayPrecios[1];
+                        document.getElementById("precioEpura").innerHTML = arrayPrecios[2];
+
+                    }else{
+                        alertify.alert("¡Ups..!", respuesta);
+                    }
+                    
+                
+                });
+        
+    </script>
+
+    <!-- FIN DE SCRIPTS PARA MONGO -->
+    <!-- Para documentación:
+
+        Las etiquetas <b> en los que se le asigna valor son únicamente para los cambios con mongo, falta con mysql -->
+
     <title>Precios</title>
 </head>
 <body>
+
+   
+
     <div class="container">
         <div class="card">
             <div class="circulo">
@@ -19,7 +68,7 @@
                     con uno y además viene hasta la planta a llenarlo recibirá
                     un descuento en el precio.
                 </p>
-                <a>PRECIO: $14 MX</a>
+                <a>PRECIO: $<b id = "precioBonafont"></b> MXN</a>
             </div>
         </div>
 
@@ -36,7 +85,7 @@
                     con uno y además viene hasta la planta a llenarlo recibirá
                     un descuento en el precio.
                 </p>
-                <a>PRECIO: $15 MX</a>
+                <a>PRECIO: $<b id = "precioCiel"></b> MXN</a>
             </div>
         </div>
 
@@ -54,7 +103,7 @@
                     con uno y además viene hasta la planta a llenarlo recibirá
                     un descuento en el precio.
                 </p>
-                <a>PRECIO: $12 MX</a>
+                <a>PRECIO: $<b id = "precioEpura"></b> MXN</a>
             </div>
         </div>
 
